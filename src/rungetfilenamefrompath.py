f8fee34217c0e51c7fa9161257231c54cd6f0223
'flybearonsky' <'zaoweiruan@sohu.com'>
2025-12-05 10:55:25 +0800

    增加自动刷新E:\My Kindle Content 目录文件功能

---
+++ src/rungetfilenamefrompath.py
@@ -0,0 +1,16 @@
+
+
+import win32com.client
+
+excel = win32com.client.Dispatch("Excel.Application")
+excel.Visible = False  # 可设为 True 观察 Excel 操作
+
+wb = excel.Workbooks.Open(r"C:\Users\dsm\Desktop\20250304.xlsm")  # 修改为实际路径
+
+try:
+    result = excel.Application.Run("ThisWorkbook.GetFilesFromFolder", r"E:\My Kindle Content",0)
+    print(r"完成 E:\My Kindle Content目录刷新")
+finally:
+    wb.Close(True)
+    excel.Quit()
+
