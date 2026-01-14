6fea0cb27d5dcc907aa504da8bc3cc750066a9e2
flybearonsky <zaoweiruan@sohu.com>
2026-01-14 17:38:30 +0800

    Fix get filename from path logic

--- src/rungetfilenamefrompath.py
+++ src/rungetfilenamefrompath.py
@@ -8,8 +8,8 @@ excel.Visible = False  # 可设为 True 观察 Excel 操作
 wb = excel.Workbooks.Open(r"C:\Users\dsm\Desktop\20250304.xlsm")  # 修改为实际路径
 
 try:
-    result = excel.Application.Run("ThisWorkbook.GetFilesFromFolder", r"E:\My Kindle Content",0)
-    print(r"完成 E:\My Kindle Content目录刷新")
+    result = excel.Application.Run("ThisWorkbook.GetFilesFromFolder", r"E:\文件存储卷_1\My Kindle Content",0)
+    print(r"完成 E:\文件存储卷_1\My Kindle Content目录刷新")
 finally:
     wb.Close(True)
     excel.Quit()
